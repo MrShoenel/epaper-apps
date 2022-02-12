0d48ad133b6b5f6b212d2df234d47518e43cff3c
Sebastian Hönel <development@hoenel.net>
2022-02-12 17:26:00 +0100

    Rename CalendarApp to CalendarMerger and override __str__()

--- src/CalendarMerger.py
+++ src/CalendarMerger.py
@@ -1,31 +1,22 @@
 import sys
 import datetime
-
 import pytz
 import requests
 from icalendar import Calendar, Event
 
 
-now = datetime.datetime.utcnow()
-now.replace(tzinfo=pytz.utc)
 today = datetime.datetime.utcnow().replace(tzinfo=pytz.utc).date()
 
 
-class CalendarApp:
+class CalendarMerger:
 
     def __init__(self):
         c = Calendar()
         c.add('prodid', '-//icalcombine//NONSGML//EN')
         c.add('version', '2.0')
         self.calendar = c
-
-
-    def addCalendar(self, url):
-        req = requests.get(url)
-        if req.status_code != 200:
-            raise Exception(f'Cannot fetch ical, status={req.status_code}')
-
-        cal = Calendar.from_ical(req.text)
+    
+    def addCalendarStr(self, cal):
         for event in cal.walk("VEVENT"):
             end = event.get('dtend')
             if end:
@@ -42,5 +33,17 @@ class CalendarApp:
                         else:
                             copied_event.add(attr, event[attr])
                     self.c.add_component(copied_event)
-
         return self
+
+
+    def addCalendar(self, url):
+        req = requests.get(url)
+        if req.status_code != 200:
+            raise Exception(f'Cannot fetch ical, status={req.status_code}')
+
+        cal = Calendar.from_ical(req.text)
+        return self.addCalendarStr(cal=cal)
+
+    def __str__(self):
+        self.calendar.to_ical(sorted=True)
+
