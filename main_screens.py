4dd5266e78a4a2443b2e077af4a19b65a1b6b9df
Sebastian Hönel <sebastian.honel@lnu.se>
2022-02-19 08:31:04 +0100

    Add main script for making screenshots

---
+++ main_screens.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import signal
+from PIL import Image
+from src.Configurator import Configurator
+from src.ScreenshotMaker import ScreenshotMaker
+
+
+if len(sys.argv) < 2:
+    raise Exception('Expecting exactly one argument: the name of the screen.')
+
+
+conf_name = sys.argv[1]
+conf = Configurator.fromJson(path='config.json').getScreenConfig(conf_name)
+
+
+if __name__ == "__main__":
+    if os.name != 'nt':
+        os.setpgrp()
+
+    try:
+        sm = ScreenshotMaker()
+        blackimg, redimg = sm.screenshot(**conf)
+        with open(file=f'{conf_name}_b.png', mode='wb') as fp:
+            blackimg.save(fp)
+        with open(file=f'{conf_name}_r.png', mode='wb') as fp:
+            redimg.save(fp)
+        
+        del sm
+    except Exception as e:
+        print(e)
+    finally:
+        if os.name == 'nt':
+            os.kill(os.getpid(), signal.SIGINT)
+        else:
+            os.killpg(os.getpgid(os.getpid()), sig=signal.SIGINT)
+
