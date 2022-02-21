88980830b0e07d5ce21d423aef2417d90c92b3ef
Sebastian Hönel <development@hoenel.net>
2022-02-21 20:30:55 +0100

    Commit of main.py that runs the entire app

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.Configurator import Configurator
+
+
+c = Configurator.fromJson(path='config.json')
+c.setupCalendar()
+c.setupStateMachines()
+c.setupBtnLedControl()
+
+c.startApi(non_blocking=False)
