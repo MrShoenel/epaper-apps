2d44d4e79bdcb76e4e5b05a1a6df1589317558e8
Sebastian Hönel <development@hoenel.net>
2022-02-21 19:18:25 +0100

    Refactor StateManager: pull out app-specific logic, make main method abstract, and add eventing

--- src/state/StateManager.py
+++ src/state/StateManager.py
@@ -2,14 +2,17 @@ import os
 from os.path import abspath, join
 from pathlib import 
 from threading import Timer
-from src.ePaper import ePaper
 from PIL import Image
+from abc import ABC, abstractmethod
+from events import Events
 
 
-class StateManager:
-    def __init__(self, config):
-        self._config = config
-        self._stateConfig = config['state_manager']
+
+class StateManager(ABC, Events):
+    __events__ = ('beforeInit', 'activateProgress', 'afterFinalize')
+
+    def __init__(self, stateConfig):
+        self._stateConfig = stateConfig
         self._state: str = None
         self._epaper = ePaper()
         self._timer: Timer = None
@@ -30,29 +33,20 @@ class StateManager:
         self._timer = Timer(interval=timeout, function=lambda: self.activate(transition='timer'))
         return self
     
-    def _initState(self, state: str):
+    async def _initState(self, state_from: str=None, transition: str=None, state_to: str, **kwargs):
         c = self._stateConfig
-        # 'state' must be one of the defined views.
-        if not state in c['views'].keys():
-            raise Exception(f'The state "{state}" is not defined.')
+        # 'state_to' must be one of the defined views.
+        if not state_to in c['views'].keys():
+            raise Exception(f'The state "{state_to}" is not defined.')
+
+        self.beforeInit(sm=self)
 
         self._unsetTimer()
 
-        # to 'init' a state means to display its rendered images on the e-paper.
-        # This happens outside of this application, and we rely on the images
-        # being present at this point.
-        # If we also use an LCD, we may also show some info there.
-        data_folder = self._config['general']['data_folder'][os.name]
-        
-        blackimg = None
-        redimg = None
-        with open(file=abspath(join(data_folder, f'{state}_b.png')) as fp:
-            blackimg = Image.open(fp=fp)
-        with open(file=abspath(join(data_folder, f'{state}_r.png')) as fp:
-            redimg = Image.open(fp=fp)
-        
-        self._epaper.display(black_img=blackimg, red_img=redimg)
-        self._state = state
+        # Now wait for the user implementation (init logic of the transition-into state):
+        await self.finalize(state_from=state_from, transition=transition, state_to=state_to, kwargs=kwargs)
+        # Now if this was successful, replace the current state:
+        self._state = state_to
 
         # Futhermore, activating means to initialize all transitions out of this
         # state. There may be zero or one 'timer'-type transition, and arbitrary
@@ -65,27 +59,45 @@ class StateManager:
         if len(timer_trans) == 1:
             tt = timer_trans[0]
             self._setTimer(timeout=float(tt['args']['timeout']))
+        
+        self.afterFinalize(sm=self)
 
         return self
     
-    def init(self):
+    async def init(self):
         """
         Transitions this state machine from an uninitialized state into
         its defined initial state.
         """
         return self.activate(transition=None)
     
-    def activate(self, transition: str=None):
+    async def activate(self, transition: str=None):
+        """
+        This method is supposed to be called by the user, in order to start (or
+        activate) a transition by name.
+        """
         if not type(transition) is str:
             if type(self._state) is str:
                 raise Exception(f'"transition" may only be None if activating the initial state.')
             return self._initState(state=self._stateConfig['initial'])
 
         # Let's check if the current state has the requested transition:
-        transitions = list(filter(function=lambda t: t.from==self._state and t.name==transition, iterable=self._stateConfig['transitions']))
+        transitions = list(filter(function=lambda t: (t.from==self._state or t.from=='*') and t.name==transition, iterable=self._stateConfig['transitions']))
         if len(transitions) == 0:
             raise Exception('The current state "{self._state}" has no transition "{transition}".')
         elif len(transition) > 1:
             raise Exception('The current state "{self._state}" has more than one transition with the name "{transition}".')
 
-        return self._initState(state=transitions[0].to)
+        trans = transitions[0]
+        return self._initState(state_from=trans['from'], transition=trans['name'], state_to=trans['to'], kwargs=trans['args'])
+
+    @abstractmethod
+    async def finalize(self, state_from: str=None, transition: str=None, state_to: str, **kwargs):
+        """
+        This method is the user implementation of what exactly happens once the
+        state machine acknowledges the transition into a new state.
+        """
+        pass
+
+
+
