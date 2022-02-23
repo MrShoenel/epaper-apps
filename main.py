d0c91aed4f1ff115246b997a289eb652780d7c7a
Sebastian Hönel <development@hoenel.net>
2022-02-23 14:38:37 +0100

    Switch initialization of buttons and state machines

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ c = Configurator.fromJson(path='config.json')
 c.setupCalendar()
 c.setupStateMachines()
 if os.name == 'posix':
-    c.initStateMachines()
     c.setupBtnLedControl()
+    c.initStateMachines()
 c.startApi(blocking=True) # If this is not blocking, the 'c' object will be collected!
