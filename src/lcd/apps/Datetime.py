9e3c4f73d562d3b354c8215d203849ec12bfce18
Sebastian Hönel <development@hoenel.net>
2022-02-23 20:46:10 +0100

    Rework Datetime app to use synchronized Threads

--- src/lcd/apps/Datetime.py
+++ src/lcd/apps/Datetime.py
@@ -1,10 +1,12 @@
 import calendar
 from datetime import datetime
+from time import sleep
 from src.lcd.TextLCD import TextLCD
 from src.lcd.apps.LcdApp import LcdApp
 from src.lcd.ScrollString import BounceString, ScrollString
-from threading import Timer, Semaphore
+from threading import Thread, Semaphore
 from src.CustomFormatter import CustomFormatter
+from typing import Callable
 
 
 def pad(s: str):
@@ -14,15 +16,21 @@ def pad(s: str):
     return s
 
 
-class Datetime(LcdApp):
+def sleep_partitioned(fn_do_sleep: Callable[[], bool], secs, by=1.0):
+    while secs > 0.0 and fn_do_sleep():
+        fraction = min(secs, by)
+        sleep(fraction)
+        secs -= fraction
 
-    def __init__(self, lcd: TextLCD, mode: str='bounce', l1interval: float=0.999, l2interval: float=4.75):
+
+class Datetime(LcdApp):
+    def __init__(self, lcd: TextLCD, mode: str='bounce', l1interval: float=0.999, l2interval: float=4.75, updateInterval: float=0.999):
         self._lcd = lcd
         self._l1interval = l1interval
         self._l2interval = l2interval
-
+        self._updateInterval = updateInterval
         self._semaphore = Semaphore(1)
-
+        
         self.scroll = mode == 'bounce'
 
         def timeFn():
@@ -35,69 +43,55 @@ class Datetime(LcdApp):
             line = f'{pad(dt.day)}. {calendar.month_abbr[dt.month].capitalize()} {pad(dt.year)}'
             return line
 
-
         self._s1 = BounceString(strFn=timeFn) if self.scroll else ScrollString(strFn=timeFn)
         self._s1Fn = self._s1.bounce if self.scroll else self._s1.scroll
         self._s2 = BounceString(strFn=dateFn) if self.scroll else ScrollString(strFn=dateFn)
         self._s2Fn = self._s2.bounce if self.scroll else self._s2.scroll
+        self._t1: Thread = None
+        self._t2: Thread = None
 
-        self._activateTimers = False
-        self._timerl1: Timer = None
-        self._timerl2: Timer = None
+        self._activate = False
 
         self.logger = CustomFormatter.getLoggerFor(self.__class__.__name__)
     
     def start(self):
-        self.stop()
+        self.stop(clear=True)
 
         self._semaphore.acquire()
-        self._activateTimers = True
-        self.logger.debug('Starting app.')
+        self._activate = True
+
+        def fnTime():
+            while self._activate:
+                self._lcd.text(line=self._s1Fn(), row=1)
+                # Continue sleeping for as long as this is active
+                sleep_partitioned(fn_do_sleep=lambda: self._activate, secs=self._l1interval, by=0.5)
+
+        self._t1 = Thread(target=fnTime)
+        self._t1.start()
+
+
+        def fnDate():
+            while self._activate:
+                self._lcd.text(line=self._s2Fn(), row=2)
+                sleep_partitioned(fn_do_sleep=lambda: self._activate, secs=self._l2interval, by=0.5)
+        
+        self._t2 = Thread(target=fnDate)
+        self._t2.start()
+
         self._semaphore.release()
-
-        self.writeTime()
-        self.writeDate()
-
         return self
     
+
     def stop(self, clear: bool=True):
         self._semaphore.acquire()
-        self._activateTimers = False
-        self.logger.debug('Stopping app.')
-        if type(self._timerl1) is Timer:
-            self._timerl1.cancel()
-        if type(self._timerl2) is Timer:
-            self._timerl2.cancel()
+        self._activate = False
+        if type(self._t1) is Thread and self._t1.is_alive():
+            self._t1.join()
+        if type(self._t2) is Thread and self._t2.is_alive():
+            self._t2.join()
         
         if clear:
             self._lcd.clear()
-
-        self._s1.reset()
-        self._s2.reset()
-
-        self._semaphore.release()
         
+        self._semaphore.release()
         return self
-    
-    def writeTime(self):
-        self._semaphore.acquire()
-        if not self._activateTimers:
-            return
-        self._lcd.text(line=self._s1Fn(), row=1)
-        if self._activateTimers:
-            self._timerl1 = Timer(interval=self._l1interval, function=self.writeTime)
-            self._timerl1.start()
-        self._semaphore.release()
-
-
-    def writeDate(self):
-        self._semaphore.acquire()
-        if not self._activateTimers:
-            return
-        self._lcd.text(line=self._s2Fn(), row=2)
-        if self._activateTimers:
-            self._timerl2 = Timer(interval=self._l2interval, function=self.writeDate)
-            self._timerl2.start()
-        self._semaphore.release()
-
-        
