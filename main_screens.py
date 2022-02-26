30e60a0a54fbc66d0b02e4331cb105d5aaf31186
Sebastian Hönel <development@hoenel.net>
2022-02-26 19:32:39 +0100

    Move lock to where it's really required

--- main_screens.py
+++ main_screens.py
@@ -16,17 +16,18 @@ conf_name = sys.argv[1]
 conf = Configurator.fromJson(path='config.json')
 screen_conf = conf.getScreenConfig(conf_name)
 data_folder = conf.getGeneralConfig()['data_folder'][os.name]
-lock = InterProcessLock(path=abspath(join(data_folder, 'write.lock')))
-lock.acquire()
 
 
 if __name__ == "__main__":
     if os.name != 'nt':
         os.setpgrp()
+    
+    lock = InterProcessLock(path=abspath(join(data_folder, 'write.lock')))
 
     try:
         sm = ScreenshotMaker(driver=conf.getGeneralConfig()['screen_driver'])
         blackimg, redimg = sm.screenshot(**screen_conf)
+        lock.acquire()
         with open(file=abspath(join(data_folder, f'{conf_name}_b.png')), mode='wb') as fp:
             blackimg.save(fp)
         with open(file=abspath(join(data_folder, f'{conf_name}_r.png')), mode='wb') as fp:
