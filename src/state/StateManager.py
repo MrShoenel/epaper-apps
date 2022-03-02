f49b5e92ae531b9bd5344bad9144eadd27d4d0e3
Sebastian Hönel <development@hoenel.net>
2022-03-02 12:07:28 +0100

    Move locking into _initState() and make finalize() private -> _finalize()

--- src/state/StateManager.py
+++ src/state/StateManager.py
@@ -39,7 +39,6 @@ class StateManager(ABC, Events):
         return self
     
     def _initState(self, state_to: str, state_from: str=None, transition: str=None, **kwargs):
-        self._semaphore.acquire()
         self.logger.debug(f'Firing event: beforeInit, before initializing state "{state_to}".')
         self._tpe.submit(lambda: self.beforeInit(sm=self, state_from=state_from, state_to=state_to, transition=transition, kwargs=kwargs))
 
@@ -48,7 +47,7 @@ class StateManager(ABC, Events):
         # Now wait for the user implementation (init logic of the transition-into state):
         self.logger.debug(f'Attempting finalization of state: "{state_to}".')
         try:
-            self.finalize(state_from=state_from, transition=transition, state_to=state_to, kwargs=kwargs)
+            self._finalize(state_from=state_from, transition=transition, state_to=state_to, kwargs=kwargs)
             # Now if this was successful, replace the current state:
             self._state = state_to
 
@@ -69,8 +68,6 @@ class StateManager(ABC, Events):
         except Exception as e:
             self.logger.error(f'Exception occurred, cannot finalize state "{state_to}": {str(e)}')
             raise e # re-throw; 'finally' will still be run
-        finally:
-            self._semaphore.release()
         
         self.logger.debug(f'Finalization complete for: "{state_to}".')
 
@@ -90,28 +87,34 @@ class StateManager(ABC, Events):
         """
         This method is supposed to be called by the user, in order to start (or
         activate) a transition by name.
+        It is a synchronized method, such that is is not possible to active the
+        same transition more than once.
         """
-        if not type(transition) is str:
-            if type(self._state) is str:
-                raise Exception(f'"transition" may only be None if activating the initial state.')
-            return self._initState(state_to=self._stateConfig['initial'])
+        try:
+            self._semaphore.acquire()
+            if not type(transition) is str:
+                if type(self._state) is str:
+                    raise Exception(f'"transition" may only be None if activating the initial state.')
+                return self._initState(state_to=self._stateConfig['initial'])
 
-        # Let's check if the current state has the requested transition:
-        transitions = list(filter(lambda t: (t['from']==self.state or t['from']=='*') and t['name']==transition, self._stateConfig['transitions']))
-        if len(transitions) == 0:
-            raise Exception('The current state "{self._state}" has no transition "{transition}".')
-        elif len(transitions) > 1:
-            raise Exception('The current state "{self._state}" has more than one transition with the name "{transition}".')
+            # Let's check if the current state has the requested transition:
+            transitions = list(filter(lambda t: (t['from']==self.state or t['from']=='*') and t['name']==transition, self._stateConfig['transitions']))
+            if len(transitions) == 0:
+                raise Exception('The current state "{self._state}" has no transition "{transition}".')
+            elif len(transitions) > 1:
+                raise Exception('The current state "{self._state}" has more than one transition with the name "{transition}".')
 
-        trans = transitions[0]
-        args = {}
-        if 'args' in trans:
-            args = trans['args']
-        return self._initState(
-            state_from=trans['from'], transition=trans['name'], state_to=trans['to'], kwargs=args)
+            trans = transitions[0]
+            args = {}
+            if 'args' in trans:
+                args = trans['args']
+            return self._initState(
+                state_from=trans['from'], transition=trans['name'], state_to=trans['to'], kwargs=args)
+        finally:
+            self._semaphore.release()
 
     @abstractmethod
-    def finalize(self, state_to: str, state_from: str=None, transition: str=None, **kwargs):
+    def _finalize(self, state_to: str, state_from: str=None, transition: str=None, **kwargs):
         """
         This method is the user implementation of what exactly happens once the
         state machine acknowledges the transition into a new state.
