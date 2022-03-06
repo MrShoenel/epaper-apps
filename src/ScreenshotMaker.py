2d0a5aa70612e1f857ad7169a13fa12a7df430ca
Sebastian Hönel <development@hoenel.net>
2022-03-06 23:41:32 +0100

    Increase sleep after get URL to 7.5

--- src/ScreenshotMaker.py
+++ src/ScreenshotMaker.py
@@ -52,7 +52,7 @@ class ScreenshotMaker:
     def screenshot(self, width: int, height: int, url: str, **kwargs) -> list[Image.Image, Image.Image]:
         self.setViewportSize(width=width, height=height)
         self.driver.get(url=url)
-        sleep(5)
+        sleep(7.5)
         
         img_bytes = self.driver.get_screenshot_as_png()
         img = Image.open(io.BytesIO(initial_bytes=img_bytes))
