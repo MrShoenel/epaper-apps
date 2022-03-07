d56e0887a6da19f02ecf0f045906b66557391a25
Sebastian Hönel <development@hoenel.net>
2022-03-07 23:42:03 +0100

    Receive config and filter keywords

--- src/news/MyNewsImpl.py
+++ src/news/MyNewsImpl.py
@@ -5,6 +5,10 @@ from functools import cmp_to_key
 
 class MyNewsImpl(NewsImpl):
 
+    def __init__(self, conf: dict[str, Any]) -> None:
+        super().__init__()
+        self.conf = conf
+
     def processItems(self, items: list[dict[str, Any]]):
         """
         We use this method to sort some of the items.
@@ -13,7 +17,23 @@ class MyNewsImpl(NewsImpl):
         items_ntv = []
         items_others = []
 
+        filter_terms = list(map(lambda s: s.lower(), self.conf['filter']))
+
+        def s(n: str=None):
+            if n is None:
+                return ''
+            return n
+
         for item in items:
+            cont = True
+            for term in map(lambda s: s.lower(), filter_terms):
+                if term in s(item['title']).lower() or term in s(item['description']).lower() or term in s(item['author']).lower():
+                    cont = False
+                    break
+            
+            if not cont:
+                continue
+
             item = item.copy()
             item['title'] = item['title'][0:item['title'].rindex(' - ')]
             if 'n-tv' in item['source']['name'].lower():
