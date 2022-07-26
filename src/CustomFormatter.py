cdd874ea9102bb866658239e0e90868d23981120
Sebastian Hönel <development@hoenel.net>
2022-07-26 15:29:02 +0200

    Add a rotating FileLogger

--- src/CustomFormatter.py
+++ src/CustomFormatter.py
@@ -1,18 +1,67 @@
 from concurrent.futures import ThreadPoolExecutor
 import logging
 from logging import LogRecord
+from typing_extensions import Self
 from events import Events
+from threading import Lock
+from os.path import exists, getsize
+
 
 
 seh_pool = ThreadPoolExecutor(max_workers=1)
 
+
+class FileLogger:
+    def __init__(self, formatter: logging.Formatter, file: str, maxLines: int) -> None:
+        self.formatter = formatter
+        self.file = file
+        self.maxLines = maxLines
+        self.lock = Lock()
+
+    def _trimFileBegin(self) -> Self:
+        lines = open(self.file).readlines()
+        if len(lines) > self.maxLines:
+            lines = lines[(len(lines) - self.maxLines):]
+            with open(file=self.file, mode='w') as f:
+                f.truncate()
+                f.writelines(lines)
+
+        return self
+
+    
+    def addRecord(self, record: LogRecord) -> Self:
+        locked = False
+        try:
+            locked = self.lock.acquire()
+            newline = '\n' if exists(self.file) and getsize(self.file) > 0 else ''
+            with open(file=self.file, mode='a') as f:
+                f.write(f'{newline}{self.formatter.format(record).strip()}')
+            return self._trimFileBegin()
+        finally:
+            if locked:
+                self.lock.release()
+        
+
+
 class StreamEventHandler(logging.StreamHandler, Events):
-    def __init__(self, stream=None):
+    def __init__(self, stream=None, fileLogger: FileLogger=None):
         logging.StreamHandler.__init__(self=self, stream=stream)
         Events.__init__(self=self, events=('onHandle'))
+        self.fileLogger = fileLogger
+        
+        def handler(record: LogRecord):
+            self.onHandle(record)
+        
+        if isinstance(fileLogger, FileLogger):
+            temp = handler
+            def handler(record: LogRecord):
+                temp(record=record)
+                self.fileLogger.addRecord(record)
+        
+        self._handler = handler
 
     def handle(self, record: LogRecord) -> bool:
-        seh_pool.submit(lambda: self.onHandle(record))
+        seh_pool.submit(lambda: self._handler(record=record))
         return super().handle(record)
 
 
@@ -21,6 +70,8 @@ class CustomFormatter(logging.Formatter):
     use_log_level = logging.INFO
 
     handler = None
+
+    file_logger = None
 
     grey = "\x1b[38;20m"
     yellow = "\x1b[33;20m"
@@ -45,6 +96,10 @@ class CustomFormatter(logging.Formatter):
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
+    
+    @staticmethod
+    def setLogFile(file: str, maxLines: int=1000):
+        CustomFormatter.file_logger = FileLogger(formatter=CustomFormatter(), file=file, maxLines=maxLines)
 
     @staticmethod
     def setLevel(level: int=logging.DEBUG):
@@ -54,7 +109,7 @@ class CustomFormatter(logging.Formatter):
     @staticmethod
     def getLoggerFor(name: str):
         if CustomFormatter.handler is None:
-            consHand = StreamEventHandler() # logging.StreamHandler()
+            consHand = StreamEventHandler(fileLogger=CustomFormatter.file_logger) # logging.StreamHandler()
             consHand.setLevel(level=CustomFormatter.use_log_level)
             consHand.setFormatter(CustomFormatter())
             CustomFormatter.handler = consHand
